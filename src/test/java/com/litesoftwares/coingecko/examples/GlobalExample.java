1f63c520ed54b13fe07aab86f2dac4d57d6935fa
Philipinho <16838612+Philipinho@users.noreply.github.com>
2019-06-10 14:43:06 +0100

    add GlobalExample

---
+++ src/test/java/com/litesoftwares/coingecko/examples/GlobalExample.java
@@ -0,0 +1,25 @@
+package com.litesoftwares.coingecko.examples;
+
+import com.litesoftwares.coingecko.CoinGeckoApiClient;
+import com.litesoftwares.coingecko.domain.Global.Global;
+import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
+
+import java.util.Map;
+
+public class GlobalExample {
+    public static void main(String[] args) {
+
+        CoinGeckoApiClient client = new CoinGeckoApiClientImpl();
+
+        Global global = client.getGlobal();
+
+        System.out.println(global);
+
+        long markets = global.getData().getMarkets();
+        System.out.println(markets);
+
+        long activeCryptoCurrencies = global.getData().getActiveCryptocurrencies();
+        System.out.println(activeCryptoCurrencies);
+
+    }
+}
