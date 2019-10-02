5c40e15fbf76428cf93151ca5206073b25808830
Philipinho <16838612+Philipinho@users.noreply.github.com>
2019-10-02 16:53:48 +0100

    lombok cleanup

--- src/main/java/com/litesoftwares/coingecko/domain/Global/Global.java
+++ src/main/java/com/litesoftwares/coingecko/domain/Global/Global.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-import java.util.Map;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 
--- src/main/java/com/litesoftwares/coingecko/domain/Status/StatusUpdates.java
+++ src/main/java/com/litesoftwares/coingecko/domain/Status/StatusUpdates.java
@@ -3,9 +3,6 @@ package com.litesoftwares.coingecko.domain.Status;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
 
 import java.util.List;
 
